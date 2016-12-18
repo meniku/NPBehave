396482b37ea08b0b0beb127650f8925d5d347a7e
Sam <sam@megidov.com>
2016-12-18 23:07:52 +0100

    Ability to launch debug window from inspector

---
+++ Editor/DebuggerEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace NPBehave
+{
+    [CustomEditor(typeof(Debugger))]
+    public class DebuggerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            GUILayout.Label("NPBehave Debugger", EditorStyles.centeredGreyMiniLabel);
+
+            if (GUILayout.Button("Open Debugger"))
+            {
+                DebuggerWindow.selectedDebugger = ((Debugger)target);
+                DebuggerWindow.selectedObject = DebuggerWindow.selectedDebugger.transform;
+                DebuggerWindow.ShowWindow();
+            }
+        }
+    }
+}
