3c0cba6755d596639ffe005b5cea7f47c306e9a4
Nils Kübler <nils-kuebler@web.de>
2019-10-09 08:28:41 +0200

    added `failOnCooldown` parameter to `Cooldown` decorator to make it immediately fail instead of waiting for the cooldown.

---
+++ Editor/Tests/Decorator/CooldownTest.cs
@@ -0,0 +1,189 @@
+﻿using NUnit.Framework;
+
+namespace NPBehave
+{
+    public class CooldownTest : Test
+    {
+		[Test]
+		public void ShouldSucceedAndSetCooldown_WhenDecorateeSucceeds()
+		{
+			MockNode child = new MockNode();
+			Cooldown sut = new Cooldown(1, child);
+			TestRoot behaviorTree = CreateBehaviorTree(sut);
+
+			// start
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// make child suceed
+			child.Finish(true);
+
+			// ensure we're stopped
+			Assert.AreEqual(Node.State.INACTIVE, sut.CurrentState);
+
+			// start again
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// ensure the child has not been started ( due to cooldown )
+			Assert.AreEqual(Node.State.INACTIVE, child.CurrentState);
+
+			// advance clock past cooldown and check that the child has been activated
+			behaviorTree.Clock.Update( 2.0f );
+			Assert.AreEqual(Node.State.ACTIVE, child.CurrentState);
+		}
+
+        [Test]
+		public void ShouldFailAndSetCooldown_WhenDecorateeFails()
+        {
+            MockNode failingChild = new MockNode();
+			Cooldown sut = new Cooldown(1, failingChild);
+            TestRoot behaviorTree = CreateBehaviorTree(sut);
+
+			// start
+            behaviorTree.Start();
+            Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// make child fail
+            failingChild.Finish(false);
+
+			// ensure we're stopped
+            Assert.AreEqual(Node.State.INACTIVE, sut.CurrentState);
+
+			// start again
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// ensure the child has not been started ( due to cooldown )
+			Assert.AreEqual(Node.State.INACTIVE, failingChild.CurrentState);
+
+			// advance clock past cooldown and check that the child has been activated
+			behaviorTree.Clock.Update( 2.0f );
+			Assert.AreEqual(Node.State.ACTIVE, failingChild.CurrentState);
+        }
+
+		[Test]
+		public void ShouldFailAndNotSetCooldown_WhenDecorateeFails_DueToParameter()
+		{
+			MockNode failingChild = new MockNode();
+			Cooldown sut = new Cooldown(1, false, true, failingChild);
+			TestRoot behaviorTree = CreateBehaviorTree(sut);
+
+			// start
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// make child fail
+			failingChild.Finish(false);
+
+			// ensure we're stopped
+			Assert.AreEqual(Node.State.INACTIVE, sut.CurrentState);
+
+			// start again
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// ensure the child has been started again ( due to cooldown not active )
+			Assert.AreEqual(Node.State.ACTIVE, failingChild.CurrentState);
+		}
+
+		[Test]
+		public void ShouldFailInsteadOfWait_WhenCooldownActive_DueToParameter()
+		{
+			MockNode child = new MockNode();
+			Cooldown sut = new Cooldown(1, false, false, true, child);
+			TestRoot behaviorTree = CreateBehaviorTree(sut);
+
+			// start
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// make child suceed
+			child.Finish(true);
+
+			// ensure we're stopped
+			Assert.AreEqual(Node.State.INACTIVE, sut.CurrentState);
+
+			// start again
+			behaviorTree.Start();
+
+			// ensure that neither this node nor the child has not been started ( due to cooldown )
+			Assert.AreEqual(Node.State.INACTIVE, sut.CurrentState);
+			Assert.AreEqual(Node.State.INACTIVE, child.CurrentState);
+
+			// advance clock past cooldown, start the tree again and check that we could be activated again
+			behaviorTree.Clock.Update( 2.0f );
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+			Assert.AreEqual(Node.State.ACTIVE, child.CurrentState);
+		}
+
+		[Test]
+		public void ShouldSucceedAndSetCooldownRightAway_WhenDecorateeSucceeds()
+		{
+			MockNode child = new MockNode();
+			Cooldown sut = new Cooldown(1, child);
+			TestRoot behaviorTree = CreateBehaviorTree(sut);
+
+			// start
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// wait 1.5 seconds
+			behaviorTree.Clock.Update( 1.5f );
+
+			// make child suceed
+			child.Finish(true);
+
+			// ensure we're stopped
+			Assert.AreEqual(Node.State.INACTIVE, sut.CurrentState);
+
+			// start again
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// ensure the child has been started as well ( due to cooldown )
+			Assert.AreEqual(Node.State.ACTIVE, child.CurrentState);
+		}
+
+		[Test]
+		public void ShouldSucceedButSetCooldownAfterDecorator_WhenDecorateeSucceeds_DueToParameter()
+		{
+			MockNode child = new MockNode();
+			Cooldown sut = new Cooldown(1, true, false, false, child);
+			TestRoot behaviorTree = CreateBehaviorTree(sut);
+
+			// start
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// wait 1.5 seconds 
+			behaviorTree.Clock.Update( 1.5f );
+
+			// make child suceed
+			child.Finish(true);
+
+			// ensure we're stopped
+			Assert.AreEqual(Node.State.INACTIVE, sut.CurrentState);
+
+			// start again
+			behaviorTree.Start();
+			Assert.AreEqual(Node.State.ACTIVE, sut.CurrentState);
+
+			// ensure the child has not been started ( due to cooldown )
+			Assert.AreEqual(Node.State.INACTIVE, child.CurrentState);
+
+			// advance clock to be at 2.0 seconds
+			behaviorTree.Clock.Update( 0.5f );
+
+			// ensure the child has not been started ( due to cooldown )
+			Assert.AreEqual(Node.State.INACTIVE, child.CurrentState);
+
+			// advance clock to be at 3 seconds
+			behaviorTree.Clock.Update( 1.0f );
+
+			// ensure the child has been started 
+			Assert.AreEqual(Node.State.ACTIVE, child.CurrentState);
+		}
+    }
+}
--- Scripts/Decorator/Cooldown.cs
+++ Scripts/Decorator/Cooldown.cs
@@ -5,12 +5,58 @@ namespace NPBehave
 
     public class Cooldown : Decorator
     {
-
-        private bool startAfterDecoratee;
+        private bool startAfterDecoratee = false;
         private bool resetOnFailiure = false;
+	    private bool failOnCooldown = false;
         private float cooldownTime = 0.0f;
         private float randomVariation = 0.05f;
         private bool isReady = true;
+
+        /// <summary>
+        /// The Cooldown decorator ensures that the branch can not be started twice within the given cooldown time.
+        /// 
+        /// The decorator can start the cooldown timer right away or wait until the child stopps, you can control this behavior with the
+        /// `startAfterDecoratee` parameter.
+        /// 
+        /// The default behavior in case the cooldown timer is active and this node is started again is, that the decorator waits until
+        /// the cooldown is reached and then executes the underlying node.
+        /// You can change this behavior with the `failOnCooldown` parameter to make the decorator immediately fail instead.
+        /// 
+        /// </summary>
+        /// <param name="cooldownTime">time until next execution</param>
+        /// <param name="randomVariation">random variation added to the cooldown time</param>
+        /// <param name="startAfterDecoratee">If set to <c>true</c> the cooldown timer is started from the point after the decoratee has been started, else it will be started right away.</param>
+        /// <param name="resetOnFailiure">If set to <c>true</c> the timer will be reset in case the underlying node fails.</param>
+        /// <param name="failOnCooldown">If currently on cooldown and this parameter is set to <c>true</c>, the decorator will immmediately fail instead of waiting for the cooldown.</param>
+        /// <param name="decoratee">Decoratee node.</param>
+        public Cooldown(float cooldownTime, float randomVariation, bool startAfterDecoratee, bool resetOnFailiure, bool failOnCooldown, Node decoratee) : base("TimeCooldown", decoratee)
+        {
+        	this.startAfterDecoratee = startAfterDecoratee;
+        	this.cooldownTime = cooldownTime;
+        	this.resetOnFailiure = resetOnFailiure;
+        	this.randomVariation = randomVariation;
+        	this.failOnCooldown = failOnCooldown;
+        	Assert.IsTrue(cooldownTime > 0f, "cooldownTime has to be set");
+        }
+
+        public Cooldown(float cooldownTime, bool startAfterDecoratee, bool resetOnFailiure, bool failOnCooldown, Node decoratee) : base("TimeCooldown", decoratee)
+        {
+        	this.startAfterDecoratee = startAfterDecoratee;
+        	this.cooldownTime = cooldownTime;
+        	this.randomVariation = cooldownTime * 0.1f;
+        	this.resetOnFailiure = resetOnFailiure;
+        	this.failOnCooldown = failOnCooldown;
+        	Assert.IsTrue(cooldownTime > 0f, "cooldownTime has to be set");
+        }
+
+        public Cooldown(float cooldownTime, float randomVariation, bool startAfterDecoratee, bool resetOnFailiure, Node decoratee) : base("TimeCooldown", decoratee)
+        {
+        	this.startAfterDecoratee = startAfterDecoratee;
+        	this.cooldownTime = cooldownTime;
+        	this.resetOnFailiure = resetOnFailiure;
+        	this.randomVariation = randomVariation;
+        	Assert.IsTrue(cooldownTime > 0f, "cooldownTime has to be set");
+        }
 
         public Cooldown(float cooldownTime, bool startAfterDecoratee, bool resetOnFailiure, Node decoratee) : base("TimeCooldown", decoratee)
         {
@@ -21,22 +67,13 @@ namespace NPBehave
             Assert.IsTrue(cooldownTime > 0f, "cooldownTime has to be set");
         }
 
-        public Cooldown(float cooldownTime, float randomVariation, bool startAfterDecoratee, bool resetOnFailiure, Node decoratee) : base("TimeCooldown", decoratee)
-        {
-            this.startAfterDecoratee = startAfterDecoratee;
-            this.cooldownTime = cooldownTime;
-            this.resetOnFailiure = resetOnFailiure;
-            this.randomVariation = randomVariation;
-            Assert.IsTrue(cooldownTime > 0f, "limit has to be set");
-        }
-
         public Cooldown(float cooldownTime, float randomVariation, Node decoratee) : base("TimeCooldown", decoratee)
         {
             this.startAfterDecoratee = false;
             this.cooldownTime = cooldownTime;
             this.resetOnFailiure = false;
             this.randomVariation = randomVariation;
-            Assert.IsTrue(cooldownTime > 0f, "limit has to be set");
+        	Assert.IsTrue(cooldownTime > 0f, "cooldownTime has to be set");
         }
 
         public Cooldown(float cooldownTime, Node decoratee) : base("TimeCooldown", decoratee)
@@ -45,9 +82,8 @@ namespace NPBehave
             this.cooldownTime = cooldownTime;
             this.resetOnFailiure = false;
             this.randomVariation = cooldownTime * 0.1f;
-            Assert.IsTrue(cooldownTime > 0f, "limit has to be set");
+        	Assert.IsTrue(cooldownTime > 0f, "cooldownTime has to be set");
         }
-
 
         protected override void DoStart()
         {
@@ -59,6 +95,13 @@ namespace NPBehave
                     Clock.AddTimer(cooldownTime, randomVariation, 0, TimeoutReached);
                 }
                 Decoratee.Start();
+            }
+            else
+            {
+                if (failOnCooldown)
+                {
+                    Stopped(false);
+                }
             }
         }
 
