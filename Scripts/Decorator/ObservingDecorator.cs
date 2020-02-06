3523ae53cf9f874efa473fa6f92b090dc27121cd
Lukas Gobelet <lulugo19@web.de>
2020-02-06 20:47:35 +0100

    Add check that lower priority nodes aren't stopped in pause state (also no restart)
    When a ObservingDecorator is Paused, the decorator stops observing.
    
    But there is also the case where another child in the parent composite is paused and the ObservingDecorator is inactive but still observes for changes to do his logic depending on the stops behavior.
    For this case there is now an additional check so that no evaluation for stopping and restarting happens in the pause state.

--- Scripts/Decorator/ObservingDecorator.cs
+++ Scripts/Decorator/ObservingDecorator.cs
@@ -78,6 +78,10 @@ namespace NPBehave
 
         protected void Evaluate()
         {
+            if (ParentNode.CurrentState == State.PAUSED)
+            {
+                return;
+            }
             if (IsActive && !IsConditionMet())
             {
                 if (stopsOnChange == Stops.SELF || stopsOnChange == Stops.BOTH || stopsOnChange == Stops.IMMEDIATE_RESTART)
