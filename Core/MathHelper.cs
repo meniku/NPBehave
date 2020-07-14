b3dbb205b90b59f672120347fb6adeefbb350654
meniku <nils-kuebler@web.de>
2020-07-14 20:17:35 +0200

    add missing file

---
+++ Core/MathHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace NPBehave
+{
+    class MathHelper
+    {
+        public static float Clamp( float value, float min, float max )
+        {
+            if ( value < min ) return min;
+            else if ( value > max ) return max;
+            else return value;
+        }
+    }
+}
